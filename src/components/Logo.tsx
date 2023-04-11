067a98df1bba26a09a4a1de36bcda8594b7aa6ff
Ugur22 <ugurertas9@gmail.com>
2023-04-11 19:57:10 +0200

    updated logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,7 +10,7 @@ export default function Logo(props) {
                 <Image
                     width={180}
                     height={100}
-                    src='img/logo-nexaDigital.svg'
+                    src='img/logo-Next.jpeg'
                     alt='logo NexaDigital'
                 />
             </Link>
